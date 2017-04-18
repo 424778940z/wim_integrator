7f83f95dc7ba4e44b3936ffbdb27ea0a83aa6753
424778940z <424778940z@gmail.com>
2017-04-18 16:35:01 -0500

    first commit

---
+++ wim_integrator/Form.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace wim_integrator
+{
+    partial class form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView_vol = new System.Windows.Forms.ListView();
+            this.textBox_search_folder = new System.Windows.Forms.TextBox();
+            this.label_search_folder = new System.Windows.Forms.Label();
+            this.button_search_folder_sel = new System.Windows.Forms.Button();
+            this.progressBar_integration = new System.Windows.Forms.ProgressBar();
+            this.button_integrate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView_vol
+            // 
+            this.listView_vol.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView_vol.Location = new System.Drawing.Point(12, 41);
+            this.listView_vol.Name = "listView_vol";
+            this.listView_vol.Size = new System.Drawing.Size(680, 381);
+            this.listView_vol.TabIndex = 0;
+            this.listView_vol.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox_search_folder
+            // 
+            this.textBox_search_folder.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_search_folder.Location = new System.Drawing.Point(101, 14);
+            this.textBox_search_folder.Name = "textBox_search_folder";
+            this.textBox_search_folder.Size = new System.Drawing.Size(512, 21);
+            this.textBox_search_folder.TabIndex = 1;
+            // 
+            // label_search_folder
+            // 
+            this.label_search_folder.AutoSize = true;
+            this.label_search_folder.Location = new System.Drawing.Point(12, 17);
+            this.label_search_folder.Name = "label_search_folder";
+            this.label_search_folder.Size = new System.Drawing.Size(83, 12);
+            this.label_search_folder.TabIndex = 2;
+            this.label_search_folder.Text = "Search Folder";
+            // 
+            // button_search_folder_sel
+            // 
+            this.button_search_folder_sel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_search_folder_sel.Location = new System.Drawing.Point(619, 12);
+            this.button_search_folder_sel.Name = "button_search_folder_sel";
+            this.button_search_folder_sel.Size = new System.Drawing.Size(73, 23);
+            this.button_search_folder_sel.TabIndex = 3;
+            this.button_search_folder_sel.Text = "Select";
+            this.button_search_folder_sel.UseVisualStyleBackColor = true;
+            this.button_search_folder_sel.Click += new System.EventHandler(this.button_search_folder_sel_Click);
+            // 
+            // progressBar_integration
+            // 
+            this.progressBar_integration.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBar_integration.Location = new System.Drawing.Point(12, 429);
+            this.progressBar_integration.Name = "progressBar_integration";
+            this.progressBar_integration.Size = new System.Drawing.Size(601, 23);
+            this.progressBar_integration.TabIndex = 4;
+            // 
+            // button_integrate
+            // 
+            this.button_integrate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_integrate.Location = new System.Drawing.Point(619, 429);
+            this.button_integrate.Name = "button_integrate";
+            this.button_integrate.Size = new System.Drawing.Size(73, 23);
+            this.button_integrate.TabIndex = 5;
+            this.button_integrate.Text = "Integrate";
+            this.button_integrate.UseVisualStyleBackColor = true;
+            this.button_integrate.Click += new System.EventHandler(this.button_integrate_Click);
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 461);
+            this.Controls.Add(this.button_integrate);
+            this.Controls.Add(this.progressBar_integration);
+            this.Controls.Add(this.button_search_folder_sel);
+            this.Controls.Add(this.label_search_folder);
+            this.Controls.Add(this.textBox_search_folder);
+            this.Controls.Add(this.listView_vol);
+            this.Name = "form";
+            this.Text = "WIM Integrator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_vol;
+        private System.Windows.Forms.TextBox textBox_search_folder;
+        private System.Windows.Forms.Label label_search_folder;
+        private System.Windows.Forms.Button button_search_folder_sel;
+        private System.Windows.Forms.ProgressBar progressBar_integration;
+        private System.Windows.Forms.Button button_integrate;
+    }
+}
+
---
+++ wim_integrator/Form.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Diagnostics;
+using Microsoft.Dism;
+
+namespace wim_integrator
+{
+    public partial class form : Form
+    {
+        public form()
+        {
+            InitializeComponent();
+            this.listView_vol.View = View.Details;
+            this.listView_vol.Columns.Add("Path");
+            this.listView_vol.Columns.Add("Volume");
+            this.listView_vol.Columns.Add("Name");
+            this.listView_vol.Columns.Add("Platform");
+            this.listView_vol.Columns.Add("Language");
+            this.listView_vol.Columns.Add("Version");
+        }
+
+        private void search_wim_file(string path)
+        {
+            List<string> install_wim_list = new List<string>();
+
+            install_wim_list = Directory.GetFiles(path, "install.wim", SearchOption.AllDirectories).ToList();
+
+            this.listView_vol.BeginUpdate();
+            DismApi.Initialize(DismLogLevel.LogErrors);
+            for (int i = 0; i < install_wim_list.Count; i++)
+            {
+                DismImageInfoCollection imageInfos = DismApi.GetImageInfo(install_wim_list[i]);
+                for(int j = 0; j < imageInfos.Count; j++)
+                {
+                    ListViewItem item_buff = new ListViewItem(install_wim_list[i]);
+
+                    //vol
+                    item_buff.SubItems.Add(imageInfos[j].ImageIndex.ToString());
+
+                    //name
+                    item_buff.SubItems.Add(imageInfos[j].ImageName);
+
+                    //platform
+                    string platform_translated;
+                    switch(imageInfos[j].Architecture)
+                    {
+                        case DismProcessorArchitecture.None:
+                            platform_translated = "None";
+                            break;
+                        case DismProcessorArchitecture.Intel:
+                            platform_translated = "x86";
+                            break;
+                        case DismProcessorArchitecture.AMD64:
+                            platform_translated = "amd64";
+                            break;
+                        case DismProcessorArchitecture.IA64:
+                            platform_translated = "ia64";
+                            break;
+                        case DismProcessorArchitecture.Neutral:
+                            platform_translated = "neutral";
+                            break;
+                        default:
+                            platform_translated = "unknow";
+                            break;
+                    }
+                    item_buff.SubItems.Add(platform_translated);
+
+                    //lang
+                    item_buff.SubItems.Add(imageInfos[j].DefaultLanguage.Name);
+
+                    //version
+                    item_buff.SubItems.Add(imageInfos[j].ProductVersion.ToString());
+
+                    this.listView_vol.Items.Add(item_buff);
+                }
+            }
+            DismApi.Shutdown();
+            this.listView_vol.EndUpdate();
+            this.listView_vol.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void integrate_wim_file(string path)
+        {
+
+            ProcessStartInfo dism_startinfo = new ProcessStartInfo("dism.exe");
+            dism_startinfo.WindowStyle = ProcessWindowStyle.Hidden;
+            dism_startinfo.UseShellExecute = false;
+            dism_startinfo.RedirectStandardOutput = true;
+            dism_startinfo.RedirectStandardError = true;
+            dism_startinfo.Arguments = "/help";
+            Process dism = Process.Start(dism_startinfo);
+            string std_out = dism.StandardError.ReadToEnd();
+            string std_err = dism.StandardOutput.ReadToEnd();
+            MessageBox.Show(std_out);
+            MessageBox.Show(std_err);
+            dism.WaitForExit();
+            MessageBox.Show(dism.ExitCode.ToString());
+
+        }
+
+        private void button_search_folder_sel_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder_sel_dialog = new System.Windows.Forms.FolderBrowserDialog();
+            //folder_sel_dialog.Description = "Select the folder";
+            folder_sel_dialog.ShowNewFolderButton = false;
+            folder_sel_dialog.SelectedPath = this.textBox_search_folder.Text;
+            if (folder_sel_dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox_search_folder.Text = folder_sel_dialog.SelectedPath;
+                search_wim_file(this.textBox_search_folder.Text);
+            }
+        }
+
+        private void button_integrate_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save_file_dialog = new System.Windows.Forms.SaveFileDialog();
+            //file_dialog.Title = "Save ingrated wim file";
+            save_file_dialog.Filter = "wim file (*.wim)|*.wim";
+            if(save_file_dialog.ShowDialog() == DialogResult.OK)
+            {
+                integrate_wim_file(save_file_dialog.FileName);
+            }
+        }
+    }
+}
